0cef5e04c9b211ed2313b54fd77ab2859449c96e
Marisua <31741111+GreenhillTeacher@users.noreply.github.com>
2022-06-01 18:59:31 -0500

    i

---
+++ age.py
@@ -0,0 +1,17 @@
+import os
+os.system('cls')
+
+yearborn = int(input("input the year you were born: "))
+currentyear = 2022
+age = currentyear - yearborn
+print (age)
+
+if(age < 50):
+    print("you are young")
+
+if(age > 50):
+    print("you are old")
+
+if(age == 50):
+    print("you are ...")
+    print(int(15/10))
